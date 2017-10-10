c2e67871d1673e941439125331a38233682a7d66
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2017-10-10 20:20:59 +0300

    make post-list a functional component

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -1,48 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 import './post.css'
 
 import Config from '../lib/config'
 
-export default class PostList extends Component {
-  render() {
-    return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-12'>
-            {this.props.posts
-              ?
-              this.props.posts.map(p => {
-                return (
-                  <div className='post' key={p.blogPostId}>
-                    <div className='row'>
-                      <div className='feat-img col-md-6'>
-                        <Link className='nav-link' to={'/post/' + p.title} >
-                          <img className='img-responsive'
-                            src={Config.api + p.image}
-                            title={p.title} 
-                            alt={p.title}/>
+const PostList = (props) => {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-12'>
+          {props.posts
+            ?
+            props.posts.map(p => {
+              return (
+                <div className='post' key={p.blogPostId}>
+                  <div className='row'>
+                    <div className='feat-img col-md-6'>
+                      <Link className='nav-link' to={'/post/' + p.title} >
+                        <img className='img-responsive'
+                          src={Config.api + p.image}
+                          title={p.title} 
+                          alt={p.title}/>
+                      </Link>
+                    </div>
+                    <div className='details col-md-6'>
+                      <h2
+                      ><Link className='nav-link' to={'/post/' + p.title} > {p.title} </Link>
+                      </h2>
+                      <p>{p.content} </p>
+                      <button className='btn pull-right'>
+                        <Link className='nav-link' to={'/post/' + p.title} > Read More!
                         </Link>
-                      </div>
-                      <div className='details col-md-6'>
-                        <h2
-                        ><Link className='nav-link' to={'/post/' + p.title} > {p.title} </Link>
-                        </h2>
-                        <p>{p.content} </p>
-                        <button className='btn pull-right'>
-                          <Link className='nav-link' to={'/post/' + p.title} > Read More!
-                          </Link>
-                        </button>
-
-                      </div>
+                      </button>
                     </div>
                   </div>
-                )
-              })
-              : null}
-          </div>
+                </div>
+              )
+            })
+            : null}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default PostList
