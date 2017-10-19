18ac7966cd7ec70721d047a2e774932bd067ae6d
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2017-10-19 22:53:18 +0300

    reminder to remove sync get config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,7 @@
 class Config {
   constructor() {
+
+    // todo - remove syncronous request for config and maybe switch to webpack proxy 
     var request = new XMLHttpRequest();
     request.open('GET', 'config.json', false);  
     request.send(null);
