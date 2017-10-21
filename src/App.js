d0742adeaef3e502c74911793747e1a2d076f850
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2017-10-21 11:34:58 +0300

    remove unused reference in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Spinner from 'react-spinkit'
 import ScrollUpButton from "react-scroll-up-button";
 import './App.css';
