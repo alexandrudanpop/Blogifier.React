96a92c02eb04672429333d57395ea2826d3f8fba
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2017-10-21 11:34:31 +0300

    add blog header css

--- src/components/blog-header.js
+++ src/components/blog-header.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 import SideNav from './side-nav'
+import './blog-header.css'
 
 const BlogHeader = (props) => {
   return (
-    <div className="page-header text-center">
+    <div className='page-header text-center'>
       <SideNav />
-      <Link to={'/'} style={{ textDecoration: 'none' }}>
-        <h1 style={{ color: 'black' }}> {props.data ? props.data.pageTitle : 'Welcome!'}
+      <div >
+        <h1>
+          <Link to={'/'} className='blog-header'>
+            {props.data ? props.data.pageTitle : 'Welcome!'}
+          </Link>
           <br />
           <small>{props.data ? ' ' + props.data.pageDescription : null}</small>
         </h1>
-      </Link>
+      </div>
     </div>
   )
 }
